a03ab74b36896dce9a136e8a6d8a84748885a98d
Blai Samitier <blai@samitier.me>
2018-02-20 21:49:58 +0100

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  	<div class="home">
+    	<img src="../assets/logo.png">
+    	<HelloWorld msg="Welcome to Your Vue.js App"/>
+  	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+	components: {
+		HelloWorld,
+	},
 })
 export default class Home extends Vue {}
 </script>
