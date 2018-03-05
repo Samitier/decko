cbb2d223eddc63f10b8e3d71d20ffe1bfbc0aaca
Blai Samitier <blai@samitier.me>
2018-03-05 13:42:19 +0100

    formatting

--- bin/server.js
+++ bin/server.js
@@ -1,32 +1,32 @@
-const   express = require("express"),
-		http = require("http"),
-		path = require("path"),
-		compression = require('compression')
+const express = require("express"),
+	http = require("http"),
+	path = require("path"),
+	compression = require('compression')
 
 class Server {
 
-    constructor() {
-        this.init()
-    }
+	constructor() {
+		this.init()
+	}
 
-    init() {
+	init() {
 		const port = process.env.PORT || 3000
 		this.app = express()
 		// Gzip
 		this.app.use(compression())
-        // Setting the static file server to the folder "public"
-        this.app.use(express.static(path.join(__dirname, "../dist")))
-        this.app.use(
-            (req, res) => res.sendFile(path.join(__dirname, "../dist/index.html"))
-        )
-        this.app.set('port', port)
-        let server = http.createServer(this.app)
-        server.listen(port)
-        server.on(
-            "listening",
-            () => console.log("Server listening on port " + port)
-        )
-    }
+		// Setting the static file server to the folder "public"
+		this.app.use(express.static(path.join(__dirname, "../dist")))
+		this.app.use(
+			(req, res) => res.sendFile(path.join(__dirname, "../dist/index.html"))
+		)
+		this.app.set('port', port)
+		const server = http.createServer(this.app)
+		server.listen(port)
+		server.on(
+			"listening",
+			() => console.log("Server listening on port " + port)
+		)
+	}
 }
 
-const server = new Server()
+new Server()
